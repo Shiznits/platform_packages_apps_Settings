85c66e7672242f79714802ae1f10b6d9c4c30451
beanstown106 <nbenis106@gmail.com>
2016-11-04 21:06:44 -0400

    add activity to launch headsup settings fragment

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -178,4 +178,5 @@ public class Settings extends SettingsActivity {
     public static class SuperSUActivity extends SettingsActivity { /* empty */ }
     public static class SuperUserActivity extends SettingsActivity { /* empty */ }
     public static class DisplayRotationActivity extends SettingsActivity { /* empty */ }
+    public static class HeadsUpSettingsActivity extends SettingsActivity { /* empty */ }
 }
